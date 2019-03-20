5f2e95f2ce655802ae4076bc017a95ba16247868
vicsanesp <48023240+vicsanesp@users.noreply.github.com>
2019-03-20 14:40:04 +0100

    Add files via upload

---
+++ Menu+Pruebas/php/menu.php
@@ -0,0 +1,35 @@
+<div>
+	<div class="example1">
+            <a href="pagina-Base.html" target="_blank">
+            <img src="imagenes/LogoRED.jpeg" alt="Logo"
+                class="ImgUS">
+                <!--<img src="imagenes/panoramica1.jpg" id="panoramica" width="1180" height="140">-->
+    		</a>
+    		<h2 class="texto-chulo">
+       			 Residencia Estudiantil Digital
+    			</h2>
+            
+    </div>
+        <div class="sticky">
+                    <!-- start nav -->
+        <nav id="menu">
+                <!-- start menu -->
+                <ul>
+                <li><a href="#">Reserva</a></li>
+                <li><a href="#">Administación</a></li>
+                <li><a href="Como-llegar.html">Como Llegar</a></li>
+                <li><a href="#">Residencia</a>
+                <!-- start menu desplegable -->
+                    <ul>
+                        <li><a href="#">Instalaciones</a></li>
+                        <li><a href="#">Conócenos</a></li>
+                        <li><a href="#">Contacto</a></li>
+                        <li><a href="#">Servicios</a></li>
+                        </ul>
+                        <!-- end menu desplegable -->
+                    </ul>
+                </li>
+                <!-- end menu -->
+                </nav>
+                <!-- end nav -->
+</div>

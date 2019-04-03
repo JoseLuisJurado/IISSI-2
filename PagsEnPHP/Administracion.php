e05826f1149618a466954faa2e7af9948a28a205
Miguel18529 <48023764+Miguel18529@users.noreply.github.com>
2019-04-03 13:25:10 +0200

    Update Administracion.php

--- PagsEnPHP/Administracion.php
+++ PagsEnPHP/Administracion.php
@@ -6,43 +6,14 @@
     <meta charset="utf-8">
     <meta title="Residencia Estudiantil Digital">
     <link rel="stylesheet" href="css/css404.css" />
+    <link rel="stylesheet" href="css/cssInicio.css" />
     <link rel="icon" type="image/jpeg" href="imagenes/LogoRED.jpeg" />
 </head>
 
-<body class="background">
-
-    <div>
-        <a href="pagina_base.html" target="_blank">
-            <img src="imagenes/LogoRED.jpeg" alt="Logo" class="ImgUS"></a>
-        <img src="imagenes/panoramica1.jpg" id="panoramica" width="1180" height="140">
-
-    </div>
-    <h2 class="texto-chulo">
-        Residencia Estudiantil Digital
-    </h2>
-    <div class="sticky">
-        <!-- start nav -->
-        <nav id="menu">
-            <!-- start menu -->
-            <ul>
-                <li><a href="formulario-reserva.html">Reserva</a></li>
-                <li><a href="#">Administación</a></li>
-                <li><a href="Como-llegar.html">Como Llegar</a></li>
-                <li><a href="Residencia.html">Residencia</a>
-                    <!-- start menu desplegable -->
-                    <ul>
-                        <li><a href="Instalaciones.html">Instalaciones</a></li>
-                        <li><a href="Conocenos.html">Conócenos</a></li>
-                        <li><a href="Contacto.html">Contacto</a></li>
-                        <li><a href="Servicios.html">Servicios</a></li>
-                    </ul>
-                    <!-- end menu desplegable -->
-            </ul>
-            </li>
-            <!-- end menu -->
-        </nav>
-        <!-- end nav -->
-    </div>
+<body>
+<?php 
+	include "php/menu.php";
+	 ?>
     <div>
         <main style="margin: 5% 20% 20% 20%;">
             <form>
@@ -64,5 +35,8 @@
             </form>
         </main>
     </div>
-
+			<?php 
+	include "php/pie.php";
+	 ?>
 </html>
+

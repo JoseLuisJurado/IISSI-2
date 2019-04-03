1acf07804884bcfd04cb9e70e6d8ee70e45e2eeb
#47522627 <thehollister93@gmail.com>
2019-04-03 13:39:09 +0200

    tocado validacionFormulario.js

--- PagsEnPHP/javascript/validacionFormulario.js
+++ PagsEnPHP/javascript/validacionFormulario.js
@@ -1,11 +1,10 @@
 
 function validacionFormulario() {
-    document.write(<h2>Esta funcionando</h2>);
     var DNI = document.forms["formularioReserva"]["DNI"].value;
     var LetrasDNI = 'TRWAGMYFPDXBNJZSQVHLCKE';
 
-    var letra = DNI.charAt(-1);
-    var numero = parseInt(DNI.slice(0,7));
+    var letra = DNI.charAt(9);
+    var numero = parseInt(DNI.slice(0,8));
 
     if(LetrasDNI.charAt(numero%24) != letra){
         alert("La letra del DNI no coincide con el número del DNI");

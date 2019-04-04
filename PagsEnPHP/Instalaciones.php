2e65f3bdcae92345021a355477874fbde2304c8c
#47522627 <thehollister93@gmail.com>
2019-04-04 10:13:43 +0200

    modificado Instalaciones

--- PagsEnPHP/Instalaciones.php
+++ PagsEnPHP/Instalaciones.php
@@ -12,76 +12,74 @@
 
 <body>
 
-    <?php 
-		include "php/menu.php";
-	 	?>
+    <?php
+    include "php/menu.php";
+    ?>
     <!-- cuerpo de la pagina de inicio solamente, si estas con otro html no uses esta clase -->
-    <div class="">
+    <div style="margin-right: 15%; margin-left: 15%;";>
         <h2 class="titulo">
             Instalaciones
         </h2>
         <h2 class="titulo2">
             Sala de Estudio
         </h2>
-        <img class="centro" style="margin-top:10px; " src="imagenes/fachada1.jpg" border="2" width="300" height="200" />
-
-        <center>
+        <img class="centro" style="margin-top:10px; text-align: center;" src="imagenes/fachada1.jpg" border="2" width="300" height="200" />
+        <p>
             La sala de estudio es un lugar de tranquilidad y perfectamente equipado para realizar trabajos o simplemente
-            poder estar en silencio<br>
+            poder estar en silencio
             para poder estudiar. En estas salas las cuales se pueden reservar para estar aislado de cualquier ruido,
-            podras encontrar enchufes para<br>
+            podras encontrar enchufes para
             usar , un televisor con sus respectivos cables de conexión externa; tanto VGA como HDMi, una pizarra, un
             proyector.
-        </center>
-
+        </p>
         <h2 class="titulo2">
             Sala Común
         </h2>
         <img class="centro" style="margin-top:10px; " src="imagenes/fachada1.jpg" border="2" width="300" height="200" />
 
-        <center>
+        <p>
             El centro dispone de una sala común en la que los residentes pueden relajarse y socializar con el resto de
-            personas que aquí reside<br>
+            personas que aquí reside
             esta sala cuenta con varios televisores, dos mesas de billar, cinco dianas de dardos, maquinas de cafe, una
-            amplia programación de<br>
+            amplia programación de
             películas para el disfrute de los asistentes. En este sitio podras venir con compañeros y amigos para
             disfrutar y descansar en compañia.
-        </center>
+        </p>
 
         <h2 class="titulo2">
             Jardines
         </h2>
         <img class="centro" style="margin-top:10px; " src="imagenes/fachada1.jpg" border="2" width="300" height="200" />
 
-        <center>
+        <p>
             El equipo de mantenimiento se enorgullece de los preciosos jardines de los que disponemos, con una vadiedad
-            de flores olorosas, ademas<br>
+            de flores olorosas, ademas
             de zonas de sombra proporcionada por nuestros árboles perfectamente cuidados, cercanos siempre a bancos
-            donde poder sentarte. Un jardin<br>
+            donde poder sentarte. Un jardin
             hermoso para pasar la tarde y respirar aire puro mientras hablas con amigos o simplemente contemplas las
             flores.
-        </center>
+        </p>
 
         <h2 class="titulo2">
             Comedor
         </h2>
         <img class="centro" style="margin-top:10px; " src="imagenes/fachada1.jpg" border="2" width="300" height="200" />
 
-        <center>
+        <p>
             El equipo de mantenimiento se enorgullece de los preciosos jardines de los que disponemos, con una vadiedad
-            de flores olorosas, ademas<br>
+            de flores olorosas, ademas
             de zonas de sombra proporcionada por nuestros árboles perfectamente cuidados, cercanos siempre a bancos
-            donde poder sentarte. Un jardin<br>
+            donde poder sentarte. Un jardin
             hermoso para pasar la tarde y respirar aire puro mientras hablas con amigos o simplemente contemplas las
             flores.
-        </center>
+        </p>
 
 
 
     </div>
-      <?php 
-	include "php/pie.php";
-	 ?>
+    <?php
+    include "php/pie.php";
+    ?>
 </body>
 
 </html>

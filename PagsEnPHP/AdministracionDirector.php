245081addcfc6158287031adba7f8b0e27b0c359
#47522627 <thehollister93@gmail.com>
2019-04-29 17:03:06 +0200

    modificando paginacion

--- PagsEnPHP/AdministracionDirector.php
+++ PagsEnPHP/AdministracionDirector.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-require_once("ConexionBD.php");
+require_once("conexionBD.php");
 $conn = crearconexionBD();
 require_once("paginacion_AdministracionDirector.php");
-
+/*
 if (isset($_SESSION["reserva"])) {
     $reserva = $_SESSION["reserva"];
     unset($_SESSION["reserva"]);
@@ -16,7 +16,34 @@ $pag_act = isset($_REQUEST["pag_act"]) && is_numeric($_REQUEST["pag_act"])? $_RE
 $pag_size = isset($_REQUEST["pag_size"])? $_REQUEST["pag_size"]:10;
 
 $pag_size = isset($_REQUEST["pag_size"]) && is_numeric($_REQUEST["pag_size"])? $_REQUEST["pag_size"]:$pag_size;
+*/
 
+
+if (isset($_SESSION["reserva"])) $reserva = $_SESSION["reserva"];
+$pag_act = isset($_GET["pag_act"]) ? (int)$_GET["pag_act"] : (isset($reserva) ? (int)$reserva["PAG_ACT"] : 1);
+$pag_size = isset($_GET["pag_size"]) ? (int)$_GET["pag_size"] : (isset($reserva) ? (int)$reserva["PAG_SIZE"] : 10);
+if ($pag_act < 1) $pag_act = 1;
+if ($pag_size < 1) $pag_size = 10;
+
+unset($_SESSION["reserva"]);
+
+$total_registros = total_consulta($conn, $consulta);
+$consultaTotal = ceil($total_registros / $pag_size);
+echo $consultaTotal.",";
+echo $total_registros.",";
+echo $pag_size.",";
+if ($pag_act > $consultaTotal) $pag_act = $consultaTotal;
+
+$reserva["PAG_ACT"] = $pag_act;
+$reserva["PAG_SIZE"] = $pag_size;
+$_SESSION["reserva"] = $reserva;
+
+$filas = consulta_paginada($conn, $consulta, $pag_act, $pag_size);
+
+cerrarConexionBD($conn);
+
+
+/*
 $_SESSION["pag_act"] = $pag_act;
 
 $consultaTotal = ceil(total_consulta($conn, $consulta) / $pag_size);
@@ -24,6 +51,7 @@ $consultaTotal = ceil(total_consulta($conn, $consulta) / $pag_size);
 $consulaPaginada = consulta_paginada($conn, $consulta, $pag_act, $pag_size);
 
 cerrarConexionBD($conn);
+*/
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -59,27 +87,30 @@ cerrarConexionBD($conn);
                 <input type="submit" value="Cambiar!">
             </form>
         </nav>
-        <?php foreach ($consulaPaginada as $pagina) {?>
+        <?php foreach ($filas as $pagina) { ?>
             <article>
-                <form method="post" action="controlarForlumario-reserva.php">
+                <form method="post" action="controlarForlumario-reserva.php" class="muestraFormulario">
+                    <div id=botones_modificacion>
+                        
+                    </div>
                     <div>
-                        <input id="DNI" name="DNI" type="text" value="<?php echo $pagina["DNI_R"];?>"/>
-                        <input id="NOMBRE" name="NOMBRE" type="text" value="<?php echo $pagina["NOMBRE"]." ".
-                        $pagina["APELLIDO1"]." ".$pagina["APELLIDO2"];?>"/>
-                        <input id="SEXO" name="SEXO" type="text" value="<?php echo $pagina["GENERO"];?>"/>
-                        <input id="FECHALLEGADA" name="FECHALLEGADA" type="text" value="<?php echo $pagina["FECHA_INICIO"];?>"/>
-                        <input id="FECHASALIDA" name="FECHASALIDA" type="text" value="<?php echo $pagina["FECHA_FIN"];?>"/>
+                        <input id="DNI" name="DNI" type="text" value="<?php echo $pagina["DNI_R"]; ?>" />
+                        <input id="NOMBRE" name="NOMBRE" type="text" value="<?php echo $pagina["NOMBRE"] . " " .
+                                                                                $pagina["APELLIDO1"] . " " . $pagina["APELLIDO2"]; ?>" />
+                        <input id="SEXO" name="SEXO" type="text" value="<?php echo $pagina["GENERO"]; ?>" />
+                        <input id="FECHALLEGADA" name="FECHALLEGADA" type="text" value="<?php echo $pagina["FECHA_INICIO"]; ?>" />
+                        <input id="FECHASALIDA" name="FECHASALIDA" type="text" value="<?php echo $pagina["FECHA_FIN"]; ?>" />
                     </div>
                 </form>
             </article>
-    <?php   } ?>
-        </main>
-        <?php
-        include "php/pie.php";
-        ?>
+        <?php   } ?>
+    </main>
+    <?php
+    include "php/pie.php";
+    ?>
 
 
 
-    </body>
+</body>
 
-    </html>
+</html>

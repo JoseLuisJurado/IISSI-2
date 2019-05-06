63cf79e1db9efa81cbabdc95e186dfa0347b9630
vicsanesp <48023240+vicsanesp@users.noreply.github.com>
2019-05-06 10:40:52 +0200

    Add files via upload

---
+++ PagsEnPHP/validacionReserva.php
@@ -0,0 +1,41 @@
+<?php 
+include_once 'gestionarContacto.php';
+?>
+<?php
+	session_start();
+
+	// Importar librerías necesarias para gestionar direcciones y géneros literarios
+	require_once("ConexionBD.php");
+
+	// Comprobar que hemos llegado a esta página porque se ha rellenado el formulario
+	if (isset($_SESSION["formulario"])) {
+		// Recogemos los datos del formulario
+		print_r($_SESSION);
+		$reservaForm['DNI'] = $_REQUEST["DNI"];
+		$reservaForm['Nombre'] = $_REQUEST["Nombre"];
+		$reservaForm['PrimerApellido'] = $_REQUEST["PrimerApellido"];
+		$reservaForm['SegundoApellido'] = $_REQUEST["SegundoApellido"];
+		$reservaForm['Sexo'] = $_REQUEST["Sexo"];
+		$reservaForm['MayoriaEdad'] = $_REQUEST["MayoriaEdad"];
+		$reservaForm['paises'] = $_REQUEST["paises"];
+		$reservaForm['Poblacion'] = $_REQUEST["Poblacion"];
+		$reservaForm['CodigoPostal'] = $_REQUEST["CodigoPostal"];
+		$reservaForm['Domicilio'] = $_REQUEST["Domicilio"];
+		$reservaForm['CorreoElectronico'] = $_REQUEST["CorreoElectronico"];
+		$reservaForm['CorreoPadre'] = $_REQUEST["CorreoPadre"];
+		$reservaForm['pass'] = $_REQUEST["pass"];
+		$reservaForm['FechaLlegada'] = $_REQUEST["FechaLlegada"];
+		$reservaForm['FechaSalida'] = $_REQUEST["FechaSalida"];
+		$reservaForm['TipoPago'] = $_REQUEST["TipoPago"];
+		$reservaForm['FormaPago'] = $_REQUEST["FormaPago"];
+		$reservaForm['pagoComedor'] = $_REQUEST["pagoComedor"];
+	
+	}
+	else // En caso contrario, vamos al formulario
+		Header("Location: Contacto.php");
+	$_SESSION["formulario"] = $reservaForm;
+	$cone = crearconexionBD();
+	insertarReserva($cone,$reservaForm);
+	cerrarConexionBD($cone);
+	
+?>

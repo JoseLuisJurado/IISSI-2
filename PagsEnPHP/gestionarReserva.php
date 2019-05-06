f3d639483b601e591b606b447d362667dc097a10
vicsanesp <48023240+vicsanesp@users.noreply.github.com>
2019-05-06 12:03:04 +0200

    Update gestionarReserva.php

--- PagsEnPHP/gestionarReserva.php
+++ PagsEnPHP/gestionarReserva.php
@@ -4,7 +4,27 @@
 	
 	$fechaLlegada = date('d/m/Y', strtotime($reservaForm["FechaLlegada"]));
 	$fechaSalida = date('d/m/Y', strtotime($reservaForm["FechaSalida"]));
-	
+	$mayor = $reservaForm["MayoriaEdad"];
+	$comedor = $reservaForm["pagoComedor"];
+	if ($mayor = "on") {
+		$mayor = "1";
+		$mayor = (integer)$mayor;
+	} else {
+		$mayor = "0";
+		$mayor = (integer)$mayor;
+	}
+	if ($comedor = "on") {
+		$comedor = "1";
+		$comedor = (integer)$comedor;
+	} else {
+		$comedor = "0";
+		$comedor = (integer)$comedor;
+	}
+	$codPostal = $reservaForm["CodigoPostal"];
+	$codPostal = (integer) $codPostal;
+	$tipos = gettype($reservaForm["Sexo"]);
+	print_r($tipos);
+	print_r($reservaForm["pass"]);
 	try {
 		$reservar = "CALL INSERTAR_RESERVA(:dni, :nombre, :ape1, :ape2, :sex, :may, :pai, :pob,
 											 :cod, :dom, :corr, :corp, :pas, :lleg, :sal, :pag, :fpag, :pagc)";
@@ -14,10 +34,10 @@
 		$stmt->bindParam(':ape1',$reservaForm["PrimerApellido"]);
 		$stmt->bindParam(':ape2',$reservaForm["SegundoApellido"]);
 		$stmt->bindParam(':sex',$reservaForm["Sexo"]);
-		$stmt->bindParam(':may',$reservaForm["MayoriaEdad"]);
+		$stmt->bindParam(':may',$mayor);
 		$stmt->bindParam(':pai',$reservaForm["paises"]);
 		$stmt->bindParam(':pob',$reservaForm["Poblacion"]);
-		$stmt->bindParam(':cod',$reservaForm["CodigoPostal"]);
+		$stmt->bindParam(':cod',$codPostal);
 		$stmt->bindParam(':dom',$reservaForm["Domicilio"]);
 		$stmt->bindParam(':corr',$reservaForm["CorreoElectronico"]);
 		$stmt->bindParam(':corp',$reservaForm["CorreoPadre"]);
@@ -26,10 +46,11 @@
 		$stmt->bindParam(':sal',$fechaSalida);
 		$stmt->bindParam(':pag',$reservaForm["TipoPago"]);
 		$stmt->bindParam(':fpag',$reservaForm["FormaPago"]);
-		$stmt->bindParam(':pagc',$reservaForm["pagoComedor"]);
+		$stmt->bindParam(':pagc',$comedor);
 		$stmt->execute();
 		
 	} catch(PDOException $e) {
 		echo $e-> getMessage();
     }
 }
+?>

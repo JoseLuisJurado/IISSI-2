4abb341712bc1273ae507f63bf38afb5968b3c91
vicsanesp <48023240+vicsanesp@users.noreply.github.com>
2019-05-06 10:40:16 +0200

    Update formulario-reserva.php

--- PagsEnPHP/formulario-reserva.php
+++ PagsEnPHP/formulario-reserva.php
@@ -1,11 +1,53 @@
-﻿<!DOCTYPE html>
+<?php
+	session_start();
+	
+	// Importar librerías necesarias para gestionar direcciones y géneros literarios
+	require_once("ConexionBD.php");
+	
+	// Si no existen datos del formulario en la sesión, se crea una entrada con valores por defecto
+	if (!isset($_SESSION["formulario"])) {
+		$formulario['DNI'] = "";
+		$formulario['Nombre'] = "";
+		$formulario['PrimerApellido'] = "";
+		$formulario['SegundoApellido'] = "";
+		$formulario['Sexo'] = "";
+		$formulario['MayoriaEdad'] = "";
+		$formulario['paises'] = "";
+		$formulario['Poblacion'] = "";
+		$formulario['CodigoPostal'] = "";
+		$formulario['Domicilio'] = "";
+		$formulario['CorreoElectronico'] = "";
+		$formulario['CorreoPadre'] = "";
+		$formulario['pass'] = "";
+		$formulario['FechaLlegada'] = "";
+		$formulario['FechaSalida'] = "";
+		$formulario['TipoPago'] = "";
+		$formulario['FormaPago'] = "";
+		$formulario['pagoComedor'] = "";
+	
+		$_SESSION["formulario"] = $formulario;
+	}
+	// Si ya existían valores, los cogemos para inicializar el formulario
+	else
+		$formulario = $_SESSION["formulario"];
+			
+	// Si hay errores de validación, hay que mostrarlos y marcar los campos (El estilo viene dado y ya se explicará)
+	if (isset($_SESSION["errores"])){
+		$errores = $_SESSION["errores"];
+		unset($_SESSION["errores"]);
+	}
+?>
+
+		<?php
+        include "php/funciones.php";
+        ?>
+
+<!DOCTYPE html>
 <html>
 
 <head>
     <meta charset="utf-8">
     <meta title="Residencia Estudiantil Digital">
-    <meta name="autor" content="Antonio Miguel González Villar">
-    <meta name="autor2" content="José Luis Jurado Ortiz">
     <link rel="stylesheet" href="css/formulario-reserva.css"/>
     <link rel="stylesheet" href="css/cssInicio.css"/>
     <script src="javascript/validacionFormulario.js" type="text/javascript"></script>
@@ -18,12 +60,9 @@
         <?php
         include "php/menu.php";
         ?>
-        <?php
-        include "php/funciones.php";
-        ?>
 
     </div>
-    <form name="formularioReserva" method="post" onsubmit="return validacionGeneral()" action="controlarForlumario-reserva.php">
+    <form name="formularioReserva" method="post" onsubmit="return validacionGeneral()" action="validacionReserva.php">
         <fieldset>
             <legend>Datos Personales</legend>
             <section>
@@ -33,17 +72,17 @@
             </section>
             <section>
                 <label>
-                    Nombre: <input type="text" name="FirstName" id="FirstName" class="margenGeneral" required>
+                    Nombre: <input type="text" name="Nombre" id="Nombre" class="margenGeneral" required>
                 </label>
             </section>
             <section>
                 <label>
-                    Primer Apellido: <input type="text" name="Primer Apellido" id="Primer Apellido" class="margenGeneral" required>
+                    Primer Apellido: <input type="text" name="PrimerApellido" id="PrimerApellido" class="margenGeneral" required>
                 </label>
             </section>
             <section>
                 <label>
-                    Segundo Apellido: <input type="text" name="Segundo Apellido" id="Segundo Apellido" class="margenGeneral" required>
+                    Segundo Apellido: <input type="text" name="SegundoApellido" id="SegundoApellido" class="margenGeneral" required>
                 </label>
             </section>
             <section class="margenGeneral">
@@ -63,7 +102,7 @@
             <legend>Otros Datos de Importancia</legend>
             <section>
                 <label for="paises"> País:
-                    <select name="pais" id="paises">
+                    <select name="paises" id="paises">
                         <?php
                         $selected = "";
                         foreach ($paises as $p => $paises) {
@@ -74,10 +113,10 @@
                     </select>
                 </label>
                 <label>
-                    Población: <input type="text" name="Población" id="Población" class="margenSexo" placeholder="Sevilla">
+                    Población: <input type="text" name="Poblacion" id="Poblacion" class="margenSexo" placeholder="Sevilla">
                 </label>
                 <label>
-                    Código Postal: <input type="text" name="CódigoPostal" id="CódigoPostal" placeholder="12345" pattern="((0[1-9]|5[0-2])|[1-4][0-9])[0-9]{3}">
+                    Código Postal: <input type="text" name="CodigoPostal" id="CodigoPostal" placeholder="12345" pattern="((0[1-9]|5[0-2])|[1-4][0-9])[0-9]{3}">
                 </label>
             </section>
             <section class="margenGeneral">
@@ -85,7 +124,7 @@
                     Domicilio: <input type="text" name="Domicilio" id="Domicilio" class="margenSexo" required>
                 </label>
                 <label>
-                    Correo electrónico: <input type="email" name="Correo electrónico" id="Correo electrónico" class="margenSexo" placeholder="aaaa@buscador.com" required>
+                    Correo electrónico: <input type="email" name="CorreoElectronico" id="CorreoElectronico" class="margenSexo" placeholder="aaaa@buscador.com" required>
                 </label>
                 <label>
                     Correo electrónico del tutor legal: <input type="email" name="CorreoPadre" id="CorreoPadre" placeholder="aaaa@buscador.com">
@@ -130,7 +169,7 @@
             </section>
             <section class="margenGeneral">
                 <label>
-                    Va a pagar el comedor?: <input type="checkbox" id="checkbox" name="pagoComedor">
+                    Va a pagar el comedor?: <input type="checkbox" id="pagoComedor" name="pagoComedor">
                 </label>
             </section>
         </fieldset>

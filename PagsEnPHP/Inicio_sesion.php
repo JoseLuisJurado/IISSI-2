700bdafbeb7f2b579f80a1a0d875dcfda738ec29
vicsanesp <48023240+vicsanesp@users.noreply.github.com>
2019-05-16 15:46:44 +0200

    Update Inicio_sesion.php

--- PagsEnPHP/Inicio_sesion.php
+++ PagsEnPHP/Inicio_sesion.php
@@ -1,183 +1,73 @@
 <?php
 	session_start();
+  	
+  	include_once("conexionBD.php");
+ 	include_once("funcionLogin.php");
 	
-	// Importar librerías necesarias para gestionar direcciones y géneros literarios
-	require_once("ConexionBD.php");
+	if (isset($_REQUEST['Enviar'])){
+		$email= $_REQUEST['Email'];
+		$pass = $_REQUEST['Contraseña'];
+		$conexion = crearconexionBD();
+		$num_usuarios = consultarUsuario($conexion,$email,$pass);
+		cerrarConexionBD($conexion);	
 	
-	// Si no existen datos del formulario en la sesión, se crea una entrada con valores por defecto
-	if (!isset($_SESSION["formulario"])) {
-		$formulario['DNI'] = "";
-		$formulario['Nombre'] = "";
-		$formulario['PrimerApellido'] = "";
-		$formulario['SegundoApellido'] = "";
-		$formulario['Sexo'] = "";
-		$formulario['MayoriaEdad'] = "";
-		$formulario['paises'] = "";
-		$formulario['Poblacion'] = "";
-		$formulario['CodigoPostal'] = "";
-		$formulario['Domicilio'] = "";
-		$formulario['CorreoElectronico'] = "";
-		$formulario['CorreoPadre'] = "";
-		$formulario['pass'] = "";
-		$formulario['FechaLlegada'] = "";
-		$formulario['FechaSalida'] = "";
-		$formulario['TipoPago'] = "";
-		$formulario['FormaPago'] = "";
-		$formulario['pagoComedor'] = "";
-	
-		$_SESSION["formulario"] = $formulario;
-	}
-	// Si ya existían valores, los cogemos para inicializar el formulario
-	else
-		$formulario = $_SESSION["formulario"];
-			
-	// Si hay errores de validación, hay que mostrarlos y marcar los campos (El estilo viene dado y ya se explicará)
-	if (isset($_SESSION["errores"])){
-		$errores = $_SESSION["errores"];
-		unset($_SESSION["errores"]);
+		if ($num_usuarios == 0)
+			$login = "error";	
+		else {
+			$_SESSION['login'] = $email;
+			Header("Location: index.php");
+		}	
 	}
 ?>
 
-		<?php
-        include "php/funciones.php";
-        ?>
-
 <!DOCTYPE html>
-<html>
-
-<head>
+<html lang="es">
+	<head>
     <meta charset="utf-8">
     <meta title="Residencia Estudiantil Digital">
-    <link rel="stylesheet" href="css/formulario-reserva.css"/>
-    <link rel="stylesheet" href="css/cssInicio.css"/>
-    <script src="javascript/validacionFormulario.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="css/cssInicio.css" />
+    <link rel="stylesheet" href="css/inicio_sesion.css" />
     <link rel="icon" type="image/png" href="imagenes/LogoRED.jpeg" />
     <Title>Residencia de estudiantes bahía</Title>
 </head>
-
 <body>
+    <?php
+    include "php/menu.php";
+    ?>
     <div>
-        <?php
-        include "php/menu.php";
-        ?>
-
+        <main class="main">
+        	       	<?php if (isset($login)) {
+		echo "<div class=\"error\">";
+		echo "Error en la contraseña o no existe el usuario.";
+		echo "</div>";
+	}	
+	?>
+            <form method="get" action="Inicio_sesion.php">
+                <fieldset class="field">
+                    <legend><u> Iniciar sesión</u></legend>
+                    <section>
+                        <label>
+                            Email: <input type="text" name="Email" id="Email" placeholder="EmailDeEjemplo@gmail.com" required>
+                        </label>
+                    </section>
+                    <section>
+                        <label>
+                            Contraseña: <input type="password" name="Contraseña" id="Contraseña" placeholder="Ejemplo: 123456abcdef" required>
+                        </label>
+                        <div>
+                            <label>
+                                ¿Aun no has hecho tu reserva? <a href="formulario-reserva.php">Reserva Aquí</a>
+                            </label>
+                        </div>
+                    </section>
+                </fieldset>
+                <input type="submit" title="Enviar" name="Enviar">
+            </form>
+        </main>
     </div>
-    <form name="formularioReserva" method="post" onsubmit="return validacionGeneral()" action="validacionReserva.php">
-        <fieldset>
-            <legend>Datos Personales</legend>
-            <section>
-                <label>
-                    DNI: <input type="text" name="DNI" id="DNI" placeholder="12345678A" maxlength="9" required>
-                </label>
-            </section>
-            <section>
-                <label>
-                    Nombre: <input type="text" name="Nombre" id="Nombre" class="margenGeneral" required>
-                </label>
-            </section>
-            <section>
-                <label>
-                    Primer Apellido: <input type="text" name="PrimerApellido" id="PrimerApellido" class="margenGeneral" required>
-                </label>
-            </section>
-            <section>
-                <label>
-                    Segundo Apellido: <input type="text" name="SegundoApellido" id="SegundoApellido" class="margenGeneral" required>
-                </label>
-            </section>
-            <section class="margenGeneral">
-                <label>
-                    Sexo:
-                    <input value="hombre" name="Sexo" id="Sexo" type="radio">Hombre
-                    <input value="mujer" name="Sexo" id="Sexo" type="radio">Mujer
-                </label>
-            </section>
-            <section class="margenGeneral">
-                <label>
-                    Eres mayor de edad?: <input type="checkbox" name="MayoriaEdad" id="MayoriaEdad">
-                </label>
-            </section>
-        </fieldset>
-        <fieldset class="margenGeneral">
-            <legend>Otros Datos de Importancia</legend>
-            <section>
-                <label for="paises"> País:
-                    <select name="paises" id="paises">
-                        <?php
-                        $selected = "";
-                        foreach ($paises as $p => $paises) {
-                            $selected = ($p == $default_paises) ? "selected" : "";
-                            echo "<option $selected value='$p'>$paises</option>";
-                        }
-                        ?>
-                    </select>
-                </label>
-                <label>
-                    Población: <input type="text" name="Poblacion" id="Poblacion" class="margenSexo" placeholder="Sevilla">
-                </label>
-                <label>
-                    Código Postal: <input type="text" name="CodigoPostal" id="CodigoPostal" placeholder="12345" pattern="((0[1-9]|5[0-2])|[1-4][0-9])[0-9]{3}">
-                </label>
-            </section>
-            <section class="margenGeneral">
-                <label>
-                    Domicilio: <input type="text" name="Domicilio" id="Domicilio" class="margenSexo" required>
-                </label>
-                <label>
-                    Correo electrónico: <input type="email" name="CorreoElectronico" id="CorreoElectronico" class="margenSexo" placeholder="aaaa@buscador.com" required>
-                </label>
-                <label>
-                    Correo electrónico del tutor legal: <input type="email" name="CorreoPadre" id="CorreoPadre" placeholder="aaaa@buscador.com">
-                </label>
-                
-                  <label for="pass">Password:
-                <input type="password" name="pass" id="pass" placeholder="Mínimo 8 caracteres" required oninput="passwordValidation(); "/>
-                </label>
-                
-                <label for="confirmpass">Confirmar Password: 
-				<input type="password" name="confirmpass" id="confirmpass" placeholder="Confirmación de contraseña"  oninput="passwordConfirmation();" required"/>
-                </label>
-                
-            </section>
-            <section>
-                <label>
-                    Fecha de llegada: <input type="date" name="FechaLlegada" id="FechaLlegada" class="margenGeneral" required>
-                </label>
-            </section>
-            <section>
-                <label>
-                    Fecha de salida: <input type="date" name="FechaSalida" id="FechaSalida" class="margenGeneral" required>
-                </label>
-            </section>
-            <section class="margenGeneral">
-                <label>
-                    Tipo de pago:
-                    <input value="mensual" name="TipoPago" id="TipoPago" type="radio">Mensual
-                    <input value="bimensual" name="TipoPago" id="TipoPago" type="radio">Bimensual
-                    <input value="cuatrimestral" name="TipoPago" id="TipoPago" type="radio">Cuatrimestral
-                    <input value="anual" name="TipoPago" id="TipoPago" type="radio">Anual
-                </label>
-            </section>
-            <section class="margenGeneral">
-                <label>
-                    Forma de pago:
-                    <input name="FormaPago" id="FormaPago" type="radio" value="efectivo">Efectivo
-                    <input name="FormaPago" id="FormaPago" type="radio" value="transferencia">Transferencia
-                    <input name="FormaPago" id="FormaPago" type="radio" value="recibo bancario">Recibo bancario
-                    <input name="FormaPago" id="FormaPago" type="radio" value="TPV">TPV
-                </label>
-            </section>
-            <section class="margenGeneral">
-                <label>
-                    Va a pagar el comedor?: <input type="checkbox" id="pagoComedor" name="pagoComedor">
-                </label>
-            </section>
-        </fieldset>
-        <div><input type="submit" value="Enviar"> </div>
-    </form>
-	<?php 
-	include "php/pie.php";
-	 ?>
+    <?php
+    include "php/pie.php";
+    ?>
 </body>
 
 </html>

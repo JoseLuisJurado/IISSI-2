38138dbc2bc13d5b32ef6a8cc5e727cb2963d33d
#47522627 <thehollister93@gmail.com>
2019-05-19 18:37:04 +0200

    algunos cambios

--- PagsEnPHP/gestionAdministracion_Usuario.php
+++ PagsEnPHP/gestionAdministracion_Usuario.php
@@ -5,12 +5,12 @@ function extraerFechasLlegadaSalida($conn, $dni)
 {
     try {
 
-        if (!isset($_SESSION["dentro"])) {
-            $_SESSION["dentro"] = "Estoy dentro de editar Libro";
-        } else {
-            unset($_SESSION["dentro"]);
-            $_SESSION["dentro"] = "Estoy dentro de editar Libro";
-        }
+        // if (!isset($_SESSION["dentro"])) {
+        //     $_SESSION["dentro"] = "Estoy dentro de editar Libro";
+        // } else {
+        //     unset($_SESSION["dentro"]);
+        //     $_SESSION["dentro"] = "Estoy dentro de editar Libro";
+        // }
 
         $consulta = "SELECT FECHA_INICIO, FECHA_FIN FROM CONTRATO_RESIDENTE NATURAL JOIN RESIDENTE WHERE DNI_R = '$dni'";
 

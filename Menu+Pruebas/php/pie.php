2d62cd01c4b6ef5d897d18c5d03217e090ebade6
#47522627 <thehollister93@gmail.com>
2019-05-22 14:21:04 +0200

    borrado General

--- Menu+Pruebas/php/pie.php
+++
@@ -1,6 +0,0 @@
-<footer>
-			<div class="pie">
-            <h5>&copy right por la Universidad de Sevilla</h5>
-            <a href="http://www.us.es/">Universidad de Sevilla</a>
-            </div>
-        </footer>

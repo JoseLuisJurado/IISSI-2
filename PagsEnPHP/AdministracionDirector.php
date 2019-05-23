6f815ea4547caa7f0d5ac6e5cc94ef0332022b4d
#47522627 <thehollister93@gmail.com>
2019-05-23 16:01:37 +0200

    algunos cambios administracionDirector

--- PagsEnPHP/AdministracionDirector.php
+++ PagsEnPHP/AdministracionDirector.php
@@ -46,7 +46,7 @@ cerrarConexionBD($conn);
 
     ?>
     <main class="cuerpo">
-    	
+
         <nav>
             <div>
                 <?php
@@ -66,76 +66,77 @@ cerrarConexionBD($conn);
             </form><br><br>
         </nav>
         <div class="cuadro">
-        <?php foreach ($filas as $pagina) { ?>
-        	
-            <article>
-            	
-                <form method="post" action="controlarAdministracionDirector.php" class="muestraFormulario">
-                    <div class="DescripcionBotones">
-                        <span class="TextoEditar">
-                            Editar
-                        </span>
-                        <span class="TextoBorrar">
-                            Borrar
-                        </span>
-                        <span class="TextoNombre">
-                            Nombre
-                        </span>
-                        <span class="TextoApellido1">
-                            Primer Apellido
-                        </span>
-                        <span class="TextoApellido2">
-                            Segundo Apellido
-                        </span>
-                        <span class="TextoSexo">
-                            Sexo
-                        </span>
-                        <span class="TextoFecha1">
-                            Fecha Llegada
-                        </span>
-                        <span class="TextoFecha2">
-                            Fecha Salida
-                        </span>
-                    </div>
+            <div class="DescripcionBotones">
+                <span class="TextoEditar">
+                    Editar
+                </span>
+                <span class="TextoBorrar">
+                    Borrar
+                </span>
+                <span class="TextoNombre">
+                    Nombre
+                </span>
+                <span class="TextoApellido1">
+                    Primer Apellido
+                </span>
+                <span class="TextoApellido2">
+                    Segundo Apellido
+                </span>
+                <span class="TextoSexo">
+                    Sexo
+                </span>
+                <span class="TextoFecha1">
+                    Fecha Llegada
+                </span>
+                <span class="TextoFecha2">
+                    Fecha Salida
+                </span>
+            </div>
 
-                    <div id="botones_modificacion" class="botones_modificacion">
-                        
-                        <?php if (isset($reserva["DNI"]) && $reserva["DNI"] == $pagina["DNI_R"]) { ?>
-                            <button id="modificar" class="modificar" type="submit" name="modificar">
-                                <img src="http://www.fileformat.info/info/unicode/char/2714/heavy_check_mark.png" width="22px">
+            <?php foreach ($filas as $pagina) { ?>
+
+                <article>
+
+                    <form method="post" action="controlarAdministracionDirector.php" class="muestraFormulario">
+
+                        <div id="botones_modificacion" class="botones_modificacion">
+
+                            <?php if (isset($reserva["DNI"]) && $reserva["DNI"] == $pagina["DNI_R"]) { ?>
+                                <button id="modificar" class="modificar" type="submit" name="modificar">
+                                    <img src="http://www.fileformat.info/info/unicode/char/2714/heavy_check_mark.png" width="22px">
+                                </button>
+                            <?php } else { ?>
+                                <button id="editar" class="editar" type="submit" name="editar">
+                                    <img src="https://www.fileformat.info/info/unicode/char/2702/black_scissors.png" width="22px">
+                                </button>
+                            <?php } ?>
+                            <button id="borrar" class="borrar" type="submit" name="borrar">
+                                <img src="http://www.fileformat.info/info/unicode/char/270f/pencil.png" width="22px" />
                             </button>
-                        <?php } else { ?>
-                            <button id="editar" class="editar" type="submit" name="editar">
-                                <img src="https://www.fileformat.info/info/unicode/char/2702/black_scissors.png" width="22px">
-                            </button>
-                        <?php } ?>
-                        <button id="borrar" class="borrar" type="submit" name="borrar">
-                            <img src="http://www.fileformat.info/info/unicode/char/270f/pencil.png" width="22px"/>
-                        </button>
-                    </div>
+                        </div>
 
-                    <div id="input_display" class="input_display">
-                        <input id="DNI" name="DNI" type="hidden" height="40px" value="<?php echo $pagina["DNI_R"]; ?>" />
-                        <input id="NOMBRE" name="NOMBRE" type="text" value="<?php echo $pagina["NOMBRE"]; ?>" />
-                        <input id="APELLIDO1" name="APELLIDO1" value="<?php echo $pagina["APELLIDO1"]; ?>" />
-                        <input id="APELLIDO2" name="APELLIDO2" value="<?php echo $pagina["APELLIDO2"]; ?>" />
-                        <input id="SEXO" name="SEXO" type="text" value="<?php echo $pagina["GENERO"]; ?>" />
-                        <input id="FECHALLEGADA" name="FECHALLEGADA" type="text" value="<?php echo $pagina["FECHA_INICIO"]; ?>" />
-                        <input id="FECHASALIDA" name="FECHASALIDA" type="text" value="<?php echo $pagina["FECHA_FIN"]; ?>" />
-                    </div><br>
+                        <div id="input_display" class="input_display">
+                            <input id="DNI" name="DNI" type="hidden" height="40px" value="<?php echo $pagina["DNI_R"]; ?>" />
+                            <input id="NOMBRE" name="NOMBRE" type="text" value="<?php echo $pagina["NOMBRE"]; ?>" />
+                            <input id="APELLIDO1" name="APELLIDO1" value="<?php echo $pagina["APELLIDO1"]; ?>" />
+                            <input id="APELLIDO2" name="APELLIDO2" value="<?php echo $pagina["APELLIDO2"]; ?>" />
+                            <input id="SEXO" name="SEXO" type="text" value="<?php echo $pagina["GENERO"]; ?>" />
+                            <input id="FECHALLEGADA" name="FECHALLEGADA" type="text" value="<?php echo $pagina["FECHA_INICIO"]; ?>" />
+                            <input id="FECHASALIDA" name="FECHASALIDA" type="text" value="<?php echo $pagina["FECHA_FIN"]; ?>" />
+                        </div><br>
 
-                    <!-- <?php foreach ($_SESSION["reserva"] as $res) echo "<br>Lo que hay dentro de reserva: " . $res ?> -->
-                    <!-- <?php if (isset($_SESSION["dentro"])) {
-                        echo $_SESSION["dentro"];
-                        unset($_SESSION["dentro"]);
-                    } else ?> -->
+                        <!-- <?php foreach ($_SESSION["reserva"] as $res) echo "<br>Lo que hay dentro de reserva: " . $res ?> -->
+                        <!-- <?php if (isset($_SESSION["dentro"])) {
+                                    echo $_SESSION["dentro"];
+                                    unset($_SESSION["dentro"]);
+                                } else ?> -->
 
-                </form>
-                
-            </article>
-            
-        <?php   } ?>
-</div>
+                    </form>
+
+                </article>
+
+            <?php   } ?>
+        </div>
     </main>
     <?php
     include "php/pie.php";

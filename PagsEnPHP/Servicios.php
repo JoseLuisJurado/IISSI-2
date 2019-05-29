24053ac0f2366a7517ef9f4b5ecb21fa1191650e
Miguel18529 <48023764+Miguel18529@users.noreply.github.com>
2019-05-29 09:53:45 +0200

    Update Servicios.php

--- PagsEnPHP/Servicios.php
+++ PagsEnPHP/Servicios.php
@@ -8,7 +8,6 @@ session_start();
     <meta charset="utf-8">
     <meta title="Residencia Estudiantil Digital">
     <link rel="stylesheet" href="css/cssInicio.css" />
-    <link rel="stylesheet" href="css/Servicios.css" />
     <link rel="icon" type="image/png" href="imagenes/LogoRED.jpeg" />
     <Title>Residencia de estudiantes bahía</Title>
 </head>
@@ -25,7 +24,7 @@ session_start();
         <h2 class="titulo2">
             Comedor
         </h2>
-        <img class="centrar"  src="imagenes/gfoto45.jpg"/>
+        <img class="centrarSer"  src="imagenes/gfoto45.jpg"/>
         <p>
             El servicio de comedor, puede ser pedido de forma opcional, con un coste adicional.
             Incluye Almuerzo completo. Este servicio estará disponible cuando un mínimo de 30 residentes lo hayan
@@ -34,7 +33,7 @@ session_start();
         <h2 class="titulo2">
             Mantenimiento
         </h2>
-        <img class="centrar2" src="imagenes/100D7VIZ003_1.jpg"/>
+        <img class="centrar2Ser" src="imagenes/100D7VIZ003_1.jpg"/>
 
         <p>
             Esta residencia consta con un eficiente servicio de mantenimiento que se encargará de la
@@ -45,7 +44,7 @@ session_start();
         <h2 class="titulo2">
             Vigilancia
         </h2>
-        <img class="centrar2" src="imagenes/Chuck Norris.jpg"/>
+        <img class="centrar2Ser" src="imagenes/Chuck Norris.jpg"/>
 
         <p>
 			El servicio de vigilancia, es el encargado de evitar que personas no autorizadas entren
@@ -55,7 +54,7 @@ session_start();
         <h2 class="titulo2">
             Limpieza
         </h2>
-        <img class="centrar2" src="imagenes/depositphotos_98500248-stock-photo-worker-in-protective-chemical-suit.jpg"/>
+        <img class="centrar2Ser" src="imagenes/depositphotos_98500248-stock-photo-worker-in-protective-chemical-suit.jpg"/>
 
         <p>
          	El contrato de la residencia incluye nuestro servicio de limpieza. Los residentes

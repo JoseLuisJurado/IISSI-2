9216d5371fb07fac9b4ae36300ab1d0f47cb4655
Miguel18529 <48023764+Miguel18529@users.noreply.github.com>
2019-05-29 13:43:37 +0200

    Update Como-llegar.php

--- PagsEnPHP/Como-llegar.php
+++ PagsEnPHP/Como-llegar.php
@@ -7,7 +7,6 @@ session_start();
 <head>
     <meta charset="utf-8">
     <meta title="Residencia Estudiantil Digital">
-    <meta name="autor" content="Antonio Miguel González Villar">
     <link rel="stylesheet" href="css/cssInicio.css" />
     <link rel="icon" type="image/png" href="imagenes/LogoRED.jpeg" />
     <Title>Residencia de estudiantes bahía</Title>

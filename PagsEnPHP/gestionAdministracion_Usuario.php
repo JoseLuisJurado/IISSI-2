11eacef6a375e15e68e3f86e9f34c9b228903373
frankys99 <45097360+frankys99@users.noreply.github.com>
2019-05-29 22:12:16 +0200

    Update gestionAdministracion_Usuario.php

--- PagsEnPHP/gestionAdministracion_Usuario.php
+++ PagsEnPHP/gestionAdministracion_Usuario.php
@@ -24,3 +24,27 @@ function extraerFechasLlegadaSalida($conn, $correo)
         header("Location: excepcion.php");
     }
 }
+
+function extraerPagoComedor($conn, $correo)
+{
+    try {
+
+        //Comprobacion de errores para saber si la funcion se ejecuta correctamente
+        // if (!isset($_SESSION["dentro"])) {
+        //     $_SESSION["dentro"] = "Estoy dentro de editar";
+        // } else {
+        //     unset($_SESSION["dentro"]);
+        //     $_SESSION["dentro"] = "Estoy dentro de editar";
+        // }
+
+        $consulta = "SELECT PAGOCOMEDOR FROM USUARIO_REGISTRADO NATURAL JOIN residente WHERE CORREO = '$correo'";
+
+        $stmt = $conn->query($consulta);
+        $result = $stmt->fetchAll();
+        return $result;
+
+    } catch (PDOException $e) {
+        $_SESSION['excepcion'] = $e->GetMessage();
+        header("Location: excepcion.php");
+    }
+}

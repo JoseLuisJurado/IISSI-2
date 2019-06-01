20526a11ac15ae8f9e47a97f7a333e3e0d625434
frankys99 <45097360+frankys99@users.noreply.github.com>
2019-06-01 13:21:47 +0200

    Update funciones.php

--- PagsEnPHP/php/funciones.php
+++ PagsEnPHP/php/funciones.php
@@ -242,3 +242,59 @@ $paises = array(
 );
 $default_paises = "ES"; //genero por defecto en pais
 ?>
+
+
+<?php
+
+//En el formulario-reserva se muestra esta funcion, especificamente en horas
+$horas = array(
+    "EL"=> "Elegir",
+	"7"=>"07:00",
+	"8"=>"08:00",
+	"9"=>"09:00",
+	"10"=>"10:00",
+	"11"=>"11:00",
+	"12"=>"12:00",
+	"13"=>"13:00",
+	"14"=>"14:00",
+	"15"=>"15:00",
+	"16"=>"16:00",
+	"17"=>"17:00",
+	"18"=>"18:00",
+	"19"=>"19:00",
+	"20"=>"20:00",
+	"21"=>"21:00",
+	"22"=>"22:00",
+	"23"=>"23:00",
+	
+);
+$default_hora = "7"; //genero por defecto en pais
+?>
+
+<?php
+
+//En el formulario-reserva se muestra esta funcion, especificamente en horas
+$horas2 = array(
+    "EL"=> "Elegir",
+	"8"=>"08:00",
+	"9"=>"09:00",
+	"10"=>"10:00",
+	"11"=>"11:00",
+	"12"=>"12:00",
+	"13"=>"13:00",
+	"14"=>"14:00",
+	"15"=>"15:00",
+	"16"=>"16:00",
+	"17"=>"17:00",
+	"18"=>"18:00",
+	"19"=>"19:00",
+	"20"=>"20:00",
+	"21"=>"21:00",
+	"22"=>"22:00",
+	"23"=>"23:00",
+	"00"=>"00:00",
+	
+);
+$default_hora2 = "8"; //genero por defecto en pais
+?>
+

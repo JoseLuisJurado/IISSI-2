c6b4577f8cd395e1875988cfd17e83a92c4b627a
#47522627 <thehollister93@gmail.com>
2019-06-01 11:59:40 +0200

    cambios varios

--- PagsEnPHP/Servicios.php
+++ PagsEnPHP/Servicios.php
@@ -18,22 +18,22 @@ session_start();
     ?>
     <div class="cuerpo2">
     <div class="margen">
-        <h2 class="titulo">
+        <h4 class="titulo">
             Servicios
-        </h2>
-        <h2 class="titulo2">
+        </h4>
+        <h4 class="titulo2">
             Comedor
-        </h2>
-        <img class="centrarSer"  src="imagenes/gfoto45.jpg"/>
+        </h4>
+        <img class="centrar2Ser"  src="http://www.cmuchaminade.com/sites/default/files/bigwig/Colegio%20Mayor%20Chaminade%20-%20Residencia%20de%20estudiantes%20Chaminade%20-%20Colegio%20Mayor%20Madrid%20-%20Residencia%20de%20estudiantes%20Madrid%20-%20comedor%203_0.jpg"/>
         <p>
             El servicio de comedor, puede ser pedido de forma opcional, con un coste adicional.
-            Incluye Almuerzo completo. Este servicio estará disponible cuando un mínimo de 30 residentes lo hayan
+            Incluye almuerzo completo. Este servicio estará disponible cuando un mínimo de 30 residentes lo hayan
             solicitado en el contrato.
         </p>
-        <h2 class="titulo2">
+        <h4 class="titulo2">
             Mantenimiento
-        </h2>
-        <img class="centrar2Ser" src="imagenes/100D7VIZ003_1.jpg"/>
+        </h4>
+        <img class="centrar2Ser" src="https://www.ithotelero.com/wp-content/uploads/2018/08/foto-6-checklist-parte-averias.png"/>
 
         <p>
             Esta residencia consta con un eficiente servicio de mantenimiento que se encargará de la
@@ -41,20 +41,20 @@ session_start();
             residente, este pagará los gastos pertinentes.
         </p>
 
-        <h2 class="titulo2">
+        <h4 class="titulo2">
             Vigilancia
-        </h2>
-        <img class="centrar2Ser" src="imagenes/Chuck Norris.jpg"/>
+        </h4>
+        <img class="centrar2Ser" src="https://www.anulimpiezas.net/wp-content/uploads/2017/02/conserjeria2.jpg"/>
 
         <p>
 			El servicio de vigilancia, es el encargado de evitar que personas no autorizadas entren
             en la residencia, así como de defender la misma de cualquier peligro externo, como ladrones o ETA.
         </p>
 
-        <h2 class="titulo2">
+        <h4 class="titulo2">
             Limpieza
-        </h2>
-        <img class="centrar2Ser" src="imagenes/depositphotos_98500248-stock-photo-worker-in-protective-chemical-suit.jpg"/>
+        </h4>
+        <img class="centrar2Ser" src="http://www.ajofrin.es/wp-content/uploads/2018/04/personal-limpieza.png"/>
 
         <p>
          	El contrato de la residencia incluye nuestro servicio de limpieza. Los residentes

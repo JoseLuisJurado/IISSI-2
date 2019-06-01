04bce99b48a4dd69cdda6e038ff59b0ac2225abe
#47522627 <thehollister93@gmail.com>
2019-06-01 21:54:01 +0200

    comentarios

--- PagsEnPHP/tramiteReserva.php
+++ PagsEnPHP/tramiteReserva.php
@@ -15,7 +15,7 @@ try {
 		// Recogemos los datos del formulario, si alguno no existe controlamos el error y lo asignamos como vacío;
 
 		//La razón por la que está puesto así es porque algunos nos daba fallo en la página.
-		//Aunque esté mal hecho, está a prueba de fallos
+		//Aunque no se haga así, está a prueba de fallos
 		if(isset($_REQUEST["DNI"])) $reservaForm['DNI'] = $_REQUEST["DNI"]; 
 
 		if(isset($_REQUEST["Nombre"])) $reservaForm['Nombre'] = $_REQUEST["Nombre"];

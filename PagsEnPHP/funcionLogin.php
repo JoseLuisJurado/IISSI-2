1613db0a4cda00ac359940ca0e9c2ea848390dde
vicsanesp <48023240+vicsanesp@users.noreply.github.com>
2019-06-01 20:19:45 +0200

    Update funcionLogin.php

--- PagsEnPHP/funcionLogin.php
+++ PagsEnPHP/funcionLogin.php
@@ -1,4 +1,5 @@
 <?php
+//función para comprobar que el inicio de sesión sea correcto o incorrecto
 function consultarUsuario($conexion,$email,$pass) {
  	$consulta = "SELECT COUNT(*) AS TOTAL FROM usuario_registrado WHERE correo=:email AND contrasena=:pass";
 	$stmt = $conexion->prepare($consulta);
